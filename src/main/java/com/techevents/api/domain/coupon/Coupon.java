29f765dac81a678766bcbae0ea71b1e795023cb8
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2025-04-26 22:48:43 -0300

    feat: create entities

---
+++ src/main/java/com/techevents/api/domain/coupon/Coupon.java
@@ -0,0 +1,29 @@
+package com.techevents.api.domain.coupon;
+
+import com.techevents.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupon")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
