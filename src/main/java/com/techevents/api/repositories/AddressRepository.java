4a8c654100b531a7a76200c7d36b9c6ae639e9fe
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2025-04-26 22:52:42 -0300

    feat: create repositories

---
+++ src/main/java/com/techevents/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.techevents.api.repositories;
+
+import com.techevents.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
---
+++ src/main/java/com/techevents/api/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.techevents.api.repositories;
+
+import com.techevents.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
